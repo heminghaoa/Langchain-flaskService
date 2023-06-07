8f5ee09b6ea89072f17df12b2b2d3c88a1c16fde
heminghaoa <minghao.he.a@gmail.com>
2023-06-07 15:57:59 +0900

    add 3.5chatapi

--- router/openai.py
+++ router/openai.py
@@ -1,6 +1,8 @@
 # router/openai.py
 from flask import Flask, jsonify
 from . import openai_app
+import os
+import openai
 
 
 @openai_app.route('/api2', methods=['GET'])
@@ -9,4 +11,22 @@ def api2():
         'message': 'API 2'
     }
     return jsonify(data)
+
+
+@openai_app.route('/chat', methods=['POST'])
+def chat():
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "Hello!what's your name?"}
+    ]
+    )
+    data = {
+        'message': completion.choices[0].message
+    }
+    print(data)
+    return jsonify(data)
+
 
